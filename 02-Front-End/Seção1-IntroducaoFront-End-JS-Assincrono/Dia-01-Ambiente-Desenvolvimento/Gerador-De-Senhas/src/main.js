48ce345d55c2ade4f28a05b517ce71a4d82901c2
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-11-14 15:45:58 -0300

    funcition nanoid para criar senha aletarória  ok, css import pelo js ok

--- 02-Front-End/Seção1-IntroducaoFront-End-JS-Assincrono/Dia-01-Ambiente-Desenvolvimento/Gerador-De-Senhas/src/main.js
+++ 02-Front-End/Seção1-IntroducaoFront-End-JS-Assincrono/Dia-01-Ambiente-Desenvolvimento/Gerador-De-Senhas/src/main.js
@@ -1,1 +1,9 @@
-alert("Heloo..")
+
+import{ nanoid } from "nanoid"; //importando biblioteca
+import './style.css'
+
+const passwwordBtnEl = document.querySelector('button');
+const displayPasswordEl = document.querySelector('h2');
+
+passwwordBtnEl.addEventListener('click', ()=> displayPasswordEl
+.innerHTML= nanoid());
