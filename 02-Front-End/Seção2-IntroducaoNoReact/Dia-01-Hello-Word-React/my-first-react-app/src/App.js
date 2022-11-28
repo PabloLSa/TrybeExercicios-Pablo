ce1196e5f5e394a90d790278a9b9ceb7481cec43
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2022-11-28 18:03:50 -0300

    adicionando minhas habilidades(treinando meu portfólio

--- 02-Front-End/Seção2-IntroducaoNoReact/Dia-01-Hello-Word-React/my-first-react-app/src/App.js
+++ 02-Front-End/Seção2-IntroducaoNoReact/Dia-01-Hello-Word-React/my-first-react-app/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React from 'react' // importando de uma biblioteca
 import ReactClass from './components/ReactClass.jsx' // importando de um caminho relativo
 import About from './components/About';
+import MinhasHabilidades from './components/MinhasHabilidades';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <About/>
+        <MinhasHabilidades/>
         <a
           className="App-link"
           href="https://reactjs.org"
