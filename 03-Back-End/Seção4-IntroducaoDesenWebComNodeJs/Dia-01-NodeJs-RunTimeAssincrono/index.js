6d163f011f1d57893d76cd148a652ac98f45bde4
pablo-Lenovo-ideapad-330-15IKB <pablolandimdesa@gmail.com>
2023-04-18 12:01:36 -0300

    ReadLine-sync
    Instalei pacote de teceiros para brincar no Log

--- 03-Back-End/Seção4-IntroducaoDesenWebComNodeJs/Dia-01-NodeJs-RunTimeAssincrono/index.js
+++ 03-Back-End/Seção4-IntroducaoDesenWebComNodeJs/Dia-01-NodeJs-RunTimeAssincrono/index.js
@@ -1,1 +1,7 @@
-console.log('Hello, world!');
+const readline = require('readline-sync');
+
+// console.log('Hello, world!');
+
+const name = readline.question('Qual seu nome? ');
+const age = readline.questionInt('Qual sua idade? ');
+console.log(`Hello, ${name}! You are ${age} years old!`);
